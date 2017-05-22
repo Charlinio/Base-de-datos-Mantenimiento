1ac6b6f1e50adab341129de4360166d42575956b
torofim <torofim@users.noreply.github.com>
2017-05-22 16:32:18 -0600

    ssss

--- src/base/de/datos/mantenimiento/BaseDeDatosMantenimiento.java
+++ src/base/de/datos/mantenimiento/BaseDeDatosMantenimiento.java
@@ -17,7 +17,7 @@ public class BaseDeDatosMantenimiento {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //puercas malnacidas
+        
     }
     
 }
